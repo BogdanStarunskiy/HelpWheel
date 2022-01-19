4ccb65925c6a3f02404473bf6969263c3732b63c
andreyskakunenko <RhfvfnjhcR>
2022-01-19 17:46:41 +0200

    commit temp

--- app/src/main/java/com/example/helpwheel/MainActivity.java
+++ app/src/main/java/com/example/helpwheel/MainActivity.java
@@ -20,13 +20,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // hello Bogdan
-        // hello Oleksandr
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -35,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-        //Hello
+
     }
 
 }
