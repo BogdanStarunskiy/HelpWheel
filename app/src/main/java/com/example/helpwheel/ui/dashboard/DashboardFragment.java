08137e6ccbdfab3c9e762194e3ebac8697f92b75
andreyskakunenko <RhfvfnjhcR>
2022-01-25 00:41:59 +0200

    fix checking

--- app/src/main/java/com/example/helpwheel/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/helpwheel/ui/dashboard/DashboardFragment.java
@@ -1,130 +1,30 @@
 package com.example.helpwheel.ui.dashboard;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
-import com.example.helpwheel.R;
 import com.example.helpwheel.databinding.FragmentDashboardBinding;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
+public class DashboardFragment extends Fragment {
 
-import javax.net.ssl.HttpsURLConnection;
-
-public class DashboardFragment extends AppCompatActivity{
-    private EditText enter_weather;
-    private Button weather_button;
-    private TextView current_weather;
     private DashboardViewModel dashboardViewModel;
     private FragmentDashboardBinding binding;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
-        return super.onCreateView(name, context, attrs);
-
-        enter_weather = findViewById(R.id.enter_city);
-        weather_button = findViewById(R.id.weather_btn);
-        current_weather = findViewById(R.id.current_weather);
-        weather_button.setOnClickListener(v -> {
-            if(enter_weather.getText().toString().trim().equals("")){
-                Toast toast = Toast.makeText(getApplicationContext(), "Enter city!", Toast.LENGTH_LONG);
-                toast.show();
-            }
-            else{
-                String user_city = enter_weather.getText().toString().trim();
-                String key = "a98ca7720a8fd711bb8548bf2373e263";
-                String url = "https://api.openweathermap.org/data/2.5/weather?q="+user_city+"&appid="+key+"&units=metric&lang=ru";
-                new GetUrlData().execute(url);
-            }
-        });
-    }
-
-    private class GetUrlData extends AsyncTask<String, String, String> {
-        @SuppressLint("SetTextI18n")
-        protected void onPreExecute(){
-            super.onPreExecute();
-            current_weather.setText("Please,wait");
-        }
-
-        @Override
-        protected String doInBackground(String... strings) {
-            HttpsURLConnection connection = null;
-            BufferedReader reader = null;
-            try {
-                URL url = new URL(strings[0]);
-                connection = (HttpsURLConnection) url.openConnection();
-                connection.connect();
-                InputStream stream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
-                while((line = reader.readLine()) != null){
-                    buffer.append(line).append("/n");
-                }
-                return buffer.toString();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally{
-                if(connection != null){
-                    connection.disconnect();
-                }
-                if(reader != null){
-                    try {
-                        reader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            return null;
-
-
-
-        }
-        @SuppressLint("SetTextI18n")
-        @Override
-        protected void onPostExecute(String result){
-            try {
-                JSONObject jsonObject = new JSONObject(result);
-                current_weather.setText("Temp "+ jsonObject.getJSONObject("main").getDouble("temp"));
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            super.onPostExecute(result);
-
-        }
-    }
-
-
-    /*public View onCreateView(@NonNull LayoutInflater inflater,
+    public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
@@ -132,12 +32,29 @@ public class DashboardFragment extends AppCompatActivity{
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+        binding.weatherBtn.setOnClickListener(v -> {
+            if (binding.enterCity.getText().toString().trim().equals("")) {
+                Toast toast = Toast.makeText(binding.getRoot().getContext(), "Enter city!", Toast.LENGTH_LONG);
+                toast.show();
+            } else {
+                String user_city = binding.enterCity.getText().toString().trim();
+                String key = "a98ca7720a8fd711bb8548bf2373e263";
+                String url = "https://api.openweathermap.org/data/2.5/weather?q=" + user_city + "&appid=" + key + "&units=metric&lang=ru";
+                dashboardViewModel.getTemperature(url);
             }
+        });
+
+
+        final TextView textView = binding.currentWeather;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), s -> {
+            String temp = "";
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                temp = "Temp " + jsonObject.getJSONObject("main").getDouble("temp");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            textView.setText(temp);
         });
         return root;
     }
@@ -146,7 +63,6 @@ public class DashboardFragment extends AppCompatActivity{
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-    }*/
-
+    }
 
 }
--- app/src/main/java/com/example/helpwheel/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/helpwheel/ui/dashboard/DashboardViewModel.java
@@ -1,8 +1,21 @@
 package com.example.helpwheel.ui.dashboard;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class DashboardViewModel extends ViewModel {
 
@@ -10,7 +23,48 @@ public class DashboardViewModel extends ViewModel {
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+    }
+
+    void getTemperature(String city) {
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        executor.execute(() -> {
+
+            //Background work here
+
+            HttpsURLConnection connection = null;
+            BufferedReader reader = null;
+            StringBuffer buffer = new StringBuffer();
+            try {
+                URL url = new URL(city);
+                connection = (HttpsURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("/n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            handler.post(() -> mText.setValue(buffer.toString()));
+        });
     }
 
     public LiveData<String> getText() {
