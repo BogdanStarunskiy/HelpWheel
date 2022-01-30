a435ff9a5e5740a2035d2c4efd6ae13ea04791ef
BogdanStarunskiy <bogdan.starunskiy@gmail.com>
2022-01-30 13:20:25 +0200

    need fix (Recycler view)

--- app/src/main/java/com/example/helpwheel/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/helpwheel/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.helpwheel.ui.home;
 
 import android.os.Bundle;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,14 +12,25 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.helpwheel.Adapter;
+import com.example.helpwheel.Model;
 import com.example.helpwheel.R;
 import com.example.helpwheel.databinding.FragmentHomeBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    RecyclerView recyclerView;
+    FloatingActionButton fab;
+    Adapter adapter;
+    List<Model> notesList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +40,15 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
 
         return binding.getRoot();
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.recycler_view_layout, container, false);
+
+        // Add the following lines to create RecyclerView
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 
     @Override
