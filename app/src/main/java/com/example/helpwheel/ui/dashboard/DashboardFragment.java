43d2958774a0f5bc9c9182eeedaf0c1cd76a4457
Oleksandr <sasha41411@gmail.com>
2022-02-15 18:36:34 +0200

    delete asynctask from weather

--- app/src/main/java/com/example/helpwheel/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/helpwheel/ui/dashboard/DashboardFragment.java
@@ -1,33 +1,30 @@
 package com.example.helpwheel.ui.dashboard;
 
-import static com.example.helpwheel.ui.dashboard.DashboardViewModel.generateURL;
-import static com.example.helpwheel.ui.dashboard.DashboardViewModel.getResponseFromURL;
-
 import android.annotation.SuppressLint;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.fragment.app.Fragment;
+
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.helpwheel.R;
 import com.example.helpwheel.databinding.FragmentDashboardBinding;
 
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.Locale;
 
 public class DashboardFragment extends Fragment {
-
     private DashboardViewModel dashboardViewModel;
     private FragmentDashboardBinding binding;
     private String currentTemperature = "";
@@ -37,70 +34,43 @@ public class DashboardFragment extends Fragment {
     private String degree_cels = "";
     private String speed = "";
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        initStrings();
+    private void showSuccessMessage() {
+        binding.currentWeather.setVisibility(View.VISIBLE);
     }
 
-    private void initStrings() {
-        currentTemperature = requireContext().getResources().getString(R.string.temperature_is);
-        feels_like = requireContext().getResources().getString(R.string.feels_like);
-        wind_speed = requireContext().getResources().getString(R.string.wind_speed);
-        weather_desc = requireContext().getResources().getString(R.string.weather_desc);
-        degree_cels = requireContext().getResources().getString(R.string.degree_cels);
-        speed = requireContext().getResources().getString(R.string.speed);
-
-    }
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        binding.weatherBtn.setOnClickListener(v -> {
-            //присваиваю преременной generatedURL значение, которое возвращает метод
-            URL generatedURL = generateURL(binding.enterCity.getText().toString().trim());
-            new WeatherQueryTask().execute(generatedURL);
-        });
-
-        return root;
-    }
     private void showErrorMessage() {
         binding.currentWeather.setVisibility(View.VISIBLE);
         binding.currentWeather.setText(R.string.error_weather);
     }
 
-    private void showSuccessMessage() {
-        binding.currentWeather.setVisibility(View.VISIBLE);
-    }
+    @SuppressLint("SetTextI18n")
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                new ViewModelProvider(this).get(DashboardViewModel.class);
 
-    class WeatherQueryTask extends AsyncTask<URL, Void, String> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            binding.weatherLoading.setVisibility(View.VISIBLE);
-        }
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        initStrings();
 
-        @Override
-        protected String doInBackground(URL... urls) {
-            String response = null;
-            try {
-                response = getResponseFromURL(urls[0]);
-            } catch (IOException e) {
-                e.printStackTrace();
+        binding.weatherBtn.setOnClickListener(v -> {
+            if (binding.enterCity.getText().toString().trim().equals("")) {
+                Toast toast = Toast.makeText(binding.getRoot().getContext(), "Enter city!", Toast.LENGTH_LONG);
+                toast.show();
+            } else {
+                String user_city = binding.enterCity.getText().toString().trim();
+                String key = "a98ca7720a8fd711bb8548bf2373e263";
+                String url = "https://api.openweathermap.org/data/2.5/weather?q=" + user_city + "&appid=" + key + "&units=metric&lang=en";
+                dashboardViewModel.getTemperature(url);
+                binding.weatherLoading.setVisibility(View.VISIBLE);
             }
-            return response;
-        }
+        });
 
-        @SuppressLint("SetTextI18n")
-        @Override
-        protected void onPostExecute(String response) {
-            if (response != null && !response.equals("")) {
+
+
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), s -> {
+            binding.weatherLoading.setVisibility(View.INVISIBLE);
+            if (s != null && !s.equals("")) {
                 String temperature = null;
                 String main_description = null;
                 String description = null;
@@ -108,7 +78,7 @@ public class DashboardFragment extends Fragment {
                 String feel_temperature = null;
 
                 try {
-                    JSONObject jsonObject = new JSONObject(response);
+                    JSONObject jsonObject = new JSONObject(s);
                     temperature = currentTemperature + " " + jsonObject.getJSONObject("main").getDouble("temp") + degree_cels;
                     feel_temperature = feels_like + " " + jsonObject.getJSONObject("main").getDouble("feels_like") + degree_cels;
                     wind = wind_speed + " " + jsonObject.getJSONObject("wind").getDouble("speed") + " "+ speed;
@@ -144,15 +114,25 @@ public class DashboardFragment extends Fragment {
 
             }
 
-        }
 
+        });
+        return root;
     }
 
+    private void initStrings() {
+        currentTemperature = requireContext().getResources().getString(R.string.temperature_is);
+        feels_like = requireContext().getResources().getString(R.string.feels_like);
+        wind_speed = requireContext().getResources().getString(R.string.wind_speed);
+        weather_desc = requireContext().getResources().getString(R.string.weather_desc);
+        degree_cels = requireContext().getResources().getString(R.string.degree_cels);
+        speed = requireContext().getResources().getString(R.string.speed);
 
+    }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
 }
