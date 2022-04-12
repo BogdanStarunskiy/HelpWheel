3592c0c1e3a1c1d23049fca315ca7727eab375ee
Oleksandr <sasha41411@gmail.com>
2022-04-12 20:59:34 +0300

    start work on Welcome Dialog

--- app/src/main/java/com/example/helpwheel/MainActivity.java
+++ app/src/main/java/com/example/helpwheel/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.helpwheel;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -14,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
-
+    private static final String MY_SETTINGS = "settings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,53 @@ public class MainActivity extends AppCompatActivity {
         }
         NavigationUI.setupWithNavController(binding.navView, navController);
 
+        if(isFirstInitShared()){
+            AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+            // Get the layout inflater
+            LayoutInflater inflater = getLayoutInflater();
 
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(inflater.inflate(R.layout.dialog_welcome, null))
+                    // Add action buttons
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            // sign in the user ...
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+            builder.create();
+        }
     }
+    private boolean isFirstInitShared() {
+        SharedPreferences sp = getSharedPreferences(MY_SETTINGS, MODE_PRIVATE);
+        boolean hasVisited = sp.getBoolean("hasVisited", false);
+
+        if (!hasVisited) {
+            SharedPreferences.Editor e = sp.edit();
+            e.putBoolean("hasVisited", true);
+            e.apply(); // applying changes
+        }
+        return !hasVisited;
+    }
+//    private void showHelloFirstDialog() {
+//
+//        new AlertDialog.Builder(MainActivity.this)
+//                .setTitle("welcome")
+//                .setMessage("welcome")
+//
+//                .setPositiveButton("ok", null)
+//                .show();
+//    }
+
+
+
 }
