8ec298fb4524e66bd70182c36ce35eede15393dc
BogdanStarunskiy <bogdan.starunskiy@gmail.com>
2022-04-16 21:43:56 +0300

    working on registration and bottom sheet dialog

--- app/src/main/java/com/example/helpwheel/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/helpwheel/ui/notifications/NotificationsFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.helpwheel.R;
+import com.example.helpwheel.databinding.BottomSheetDialogBinding;
 import com.example.helpwheel.databinding.FragmentNotificationsBinding;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
